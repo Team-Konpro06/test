08e418ef1d7d6d8ce33a0762b23f9ae3f9dc9332
agungsoedarbe <soedarbeagung@gmail.com>
2017-12-15 14:49:07 +0700

    test

---
+++ break.py
@@ -0,0 +1,25 @@
+#for i in range(0,5):
+#	if i == 3:
+#		print("angka 3 didapat")
+#		break
+#	print(i)
+	
+#i = 0
+#while i < 5:
+#	if i == 3:
+#		print("angka 3 terambil")
+#		break
+#	print(i)
+#	i+=10
+	
+#for i in range(5):
+#	if i == 3:
+#		pass
+#		print("ini pass")
+#	print("angka sekarang : ",i)
+	
+for i in range(5):
+	if i == 3:
+		continue
+		print("ini continue")
+	print("angka sekarang : ",i)
